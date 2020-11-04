5d20632a5926f208003a2347343a28f706dd6b85
zhiHao <15670520087@163.com>
2020-11-04 09:59:01 +0800

    迭代反转链表掌握了

--- src/main/java/com/zh/leetcode/editor/cn/ReverseLinkedList.java
+++ src/main/java/com/zh/leetcode/editor/cn/ReverseLinkedList.java
@@ -32,7 +32,7 @@ public class ReverseLinkedList{
  * }
  */
 class Solution {
-    public ListNode reverseList(ListNode head) {
+    /*public ListNode reverseList(ListNode head) {
         if(head == null){
             return head;
         }
@@ -51,6 +51,22 @@ class Solution {
             cur = cur.next;
         }
         return listNode;
+    }*/
+
+
+    public ListNode reverseList(ListNode head) {
+        //上面是栈 这次试试迭代
+        if(head == null){
+            return head;
+        }
+        ListNode pre = null;
+        while(head!=null){
+             ListNode tmp = head.next;
+             head.next = pre;
+             pre = head;
+             head = tmp;
+        }
+        return pre;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
